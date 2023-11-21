5009099118a949ff75ba379c09d8300491b22f63
kevrrana <145452208+kevrrana@users.noreply.github.com>
2023-11-21 10:49:38 +0300

    Add files via upload

---
+++ class-ornek/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace class_ornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Kitap kitap1=new Kitap();
+            kitap1.turu = "Roman";
+            kitap1.yazari = "Arslı Arslan";
+            kitap1.sayfasayisi = 624;
+            kitap1.kitapismi = "Sokak Nöbetçileri 3";
+            kitap1.yayintarihi = 2023;
+            label1.Text = kitap1.turu + " " + kitap1.yazari + " " + kitap1.sayfasayisi +" "+kitap1.kitapismi+" "+kitap1.yayintarihi;
+
+
+
+        }
+    }
+    }
+
---
+++ class-ornek/Kitap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_ornek
+{
+    public class Kitap
+    {
+        public string turu;
+        public string yazari;
+        public int sayfasayisi;
+        public string kitapismi;
+        public int yayintarihi;
+    }
+}
